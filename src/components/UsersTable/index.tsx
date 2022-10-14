30761cfd36170ff2767d5b97ac4a95436eaab22b
Matheus Telles <business.mwtelles@gmail.com>
2022-10-14 18:47:24 -0300

    feat: tabela de usuários

--- src/components/UsersTable/index.tsx
+++ src/components/UsersTable/index.tsx
@@ -3,101 +3,100 @@ import User from "../../assets/images/user_photo.png";
 
 function UsersTable() {
   return (
-      <div className="sm:px-6 w-full">
-          <div className="bg-white py-4 md:py-7 px-4 md:px-8 xl:px-10">
-              <div className="sm:flex items-center justify-between">
-                  <div className="flex items-center">
-                      
-                  </div>
-                  <div className="flex">
-                      <div className="focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600 mt-4 sm:mt-0 inline-flex items-center justify-center px-6 py-3 bg-indigo-700 hover:bg-indigo-600 focus:outline-none rounded-full">
-                          <button className="">
-                              <p className="text-sm font-medium leading-none text-white">Add Usuário</p>
-                          </button>
-                      </div>
-                  </div>
-              </div>
-              <div className="mt-7 overflow-x-auto">
-                  <table className="w-full whitespace-nowrap">
-                      <tbody>
-                          <tr tabindex="0" className="focus:outline-none h-16 border border-gray-100">
-                              <td>
-                                  <div className="ml-5">
-                                      <div className="bg-gray-200 rounded-sm w-5 h-5 flex flex-shrink-0 justify-center items-center relative">
-                                          <input placeholder="checkbox" type="checkbox" className="focus:opacity-100 checkbox opacity-0 absolute cursor-pointer w-full h-full" />
-                                          <div className="check-icon hidden bg-indigo-700 text-white rounded-full">
-                                              <svg className="icon icon-tabler icon-tabler-check" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                                  <path stroke="none" d="M0 0h24v24H0z"></path>
-                                                  <path d="M5 12l5 5l10 -10"></path>
-                                              </svg>
-                                          </div>
-                                      </div>
-                                  </div>
-                              </td>
-                              <td className="">
-                                  <div className="flex items-center pl-4">
-                                      <div className="w-10 h-9 mr-2 sm:mr-2">
-                                          <img className="rounded-full w-9 h-9 object-fit" src={User} alt="Foto do Usuário" />
-                                      </div>
-                                      <div className="font-medium text-gray-800">Matheus Telles</div>
-                                  </div>
-                              </td>
-                              <td className="pl-24">
-                                  <div className="flex items-center">
-                                      <p className="text-sm leading-none text-gray-600 ml-2">Coordenador</p>
-                                  </div>
-                              </td>
-                              <td className="pl-5">
-
-                                  <div className="flex items-center">
-                                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                                          <path d="M3.33331 17.4998V6.6665C3.33331 6.00346 3.59671 5.36758 4.06555 4.89874C4.53439 4.4299 5.17027 4.1665 5.83331 4.1665H14.1666C14.8297 4.1665 15.4656 4.4299 15.9344 4.89874C16.4033 5.36758 16.6666 6.00346 16.6666 6.6665V11.6665C16.6666 12.3295 16.4033 12.9654 15.9344 13.4343C15.4656 13.9031 14.8297 14.1665 14.1666 14.1665H6.66665L3.33331 17.4998Z" stroke="#52525B" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path>
-                                          <path d="M10 9.1665V9.17484" stroke="#52525B" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path>
-                                          <path d="M6.66669 9.1665V9.17484" stroke="#52525B" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path>
-                                          <path d="M13.3333 9.1665V9.17484" stroke="#52525B" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path>
-                                      </svg>
-                                      <p className="text-sm leading-none text-gray-600 ml-2">5</p>
-                                  </div>
-                              </td>
-                              <td className="pl-5">
-                                  <div className="flex items-center">
-                                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                                          <path d="M7.5 5H16.6667" stroke="#52525B" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path>
-                                          <path d="M7.5 10H16.6667" stroke="#52525B" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path>
-                                          <path d="M7.5 15H16.6667" stroke="#52525B" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path>
-                                          <path d="M4.16669 5V5.00667" stroke="#52525B" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path>
-                                          <path d="M4.16669 10V10.0067" stroke="#52525B" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path>
-                                          <path d="M4.16669 15V15.0067" stroke="#52525B" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path>
-                                      </svg>
-                                      <p className="text-sm leading-none text-gray-600 ml-2">1</p>
-                                  </div>
-                              </td>
-                              <td>
-                                  <div className="relative px-5 pt-2">
-                                      <button className="focus:ring-2 rounded-md focus:outline-none" aria-label="option">
-                                          <svg className="dropbtn" onclick="dropdownFunction(this)" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                                              <path d="M4.16667 10.8332C4.62691 10.8332 5 10.4601 5 9.99984C5 9.5396 4.62691 9.1665 4.16667 9.1665C3.70643 9.1665 3.33334 9.5396 3.33334 9.99984C3.33334 10.4601 3.70643 10.8332 4.16667 10.8332Z" stroke="#9CA3AF" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path>
-                                              <path d="M10 10.8332C10.4602 10.8332 10.8333 10.4601 10.8333 9.99984C10.8333 9.5396 10.4602 9.1665 10 9.1665C9.53976 9.1665 9.16666 9.5396 9.16666 9.99984C9.16666 10.4601 9.53976 10.8332 10 10.8332Z" stroke="#9CA3AF" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path>
-                                              <path d="M15.8333 10.8332C16.2936 10.8332 16.6667 10.4601 16.6667 9.99984C16.6667 9.5396 16.2936 9.1665 15.8333 9.1665C15.3731 9.1665 15 9.5396 15 9.99984C15 10.4601 15.3731 10.8332 15.8333 10.8332Z" stroke="#9CA3AF" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path>
-                                          </svg>
-                                      </button>
-                                      <div className="dropdown-content bg-white shadow w-24 absolute z-30 right-0 mr-6 hidden">
-                                          <div tabindex="0" className="focus:outline-none focus:text-indigo-600 text-xs w-full hover:bg-indigo-700 py-4 px-4 cursor-pointer hover:text-white">
-                                              <p>Editar</p>
-                                          </div>
-                                          <div tabindex="0" className="focus:outline-none focus:text-indigo-600 text-xs w-full hover:bg-indigo-700 py-4 px-4 cursor-pointer hover:text-white">
-                                              <p>Deletar</p>
-                                          </div>
-                                      </div>
-                                  </div>
-                              </td>
-                          </tr>
-
-                      </tbody>
-                  </table>
-              </div>
-          </div>
-      </div>
+    <div className="flex flex-col justify-between rounded-md bg-white p-7">
+    <div className="flex items-center justify-between">
+      <h2 className="text-[20px] font-medium text-gray-900">Usuários</h2>
+      <button className="inline-flex items-center gap-x-1 rounded-md bg-primary py-2 px-4 text-sm text-white hover:text-white hover:bg-gray-900">
+        <svg
+          fill="none"
+          viewBox="0 0 24 24"
+          className="h-6 w-6 stroke-current"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="1.5"
+            d="M12 5.75v12.5M18.25 12H5.75"
+          ></path>
+        </svg>
+        <span>Add Usuário</span>
+      </button>
+    </div>
+    <table className="mt-4">
+      <thead>
+        <tr>
+          <td className="py-1 text-sm text-gray-400">Id</td>
+          <td className="py-1 text-sm text-gray-400">Data de cadastro</td>
+          <td className="py-1 text-sm text-gray-400">Nome</td>
+          <td className="py-1 text-sm text-gray-400">Função</td>
+          <td className="py-1 text-sm text-gray-400">Status</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr className="border-b border-gray-700 last:border-none">
+          <td className="py-4">
+            <span className="text-sm font-medium text-gray-400">
+              #01
+            </span>
+          </td>
+          <td className="py-4">
+            <span className="text-sm text-gray-400">Out 01, 2022</span>
+          </td>
+          <td className="py-4">
+            <div className="flex items-center gap-x-2">
+              <img
+                src="https://avatars.githubusercontent.com/u/49735512?v=4"
+                alt=""
+                className="h-7 w-7 rounded-full object-cover"
+              />
+              <span className="text-sm text-gray-400">Matheus Telles</span>
+            </div>
+          </td>
+          <td className="py-4">
+            <span className="text-sm text-gray-400">Administrador</span>
+          </td>
+          <td className="py-4">
+          <div className="flex items-center justify-center gap-x-2 rounded-md border py-2 px-1 border-indigo-400/10 bg-indigo-700/20 text-indigo-400">
+              <span className="h-2 w-2 rounded-full bg-indigo-400"></span>
+              <span className="text-xs">Cadastrado</span>
+            </div>
+          </td>
+        </tr>
+        <tr className="border-b border-gray-700 last:border-none">
+          <td className="py-4">
+            <span className="text-sm font-medium text-gray-400">
+              #02
+            </span>
+          </td>
+          <td className="py-4">
+            <span className="text-sm text-gray-400">Out 11, 2022</span>
+          </td>
+          <td className="py-4">
+            <div className="flex items-center gap-x-2">
+              <img
+                src="https://avatars.githubusercontent.com/u/49735526?v=4"
+                alt=""
+                className="h-7 w-7 rounded-full object-cover"
+              />
+              <span className="text-sm text-gray-400">
+                Pedro H. Vasconcelos
+              </span>
+            </div>
+          </td>
+          <td className="py-4">
+            <span className="text-sm text-gray-400">Coordenador</span>
+          </td>
+          <td className="py-4">
+            <div className="flex items-center justify-center gap-x-2 rounded-md border py-2 px-1 border-red-400/10 bg-red-700/20 text-ted-400">
+              <span className="h-2 w-2 rounded-full bg-red-400"></span>
+              <span className="text-xs">Pendente</span>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
   )
 }
 
